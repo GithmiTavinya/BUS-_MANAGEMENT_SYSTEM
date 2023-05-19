d1726dffccc6da7943ad566b4b7bf4a61bceee2e
GithmiTavinya <it20607814@my.sliit.lk>
2023-05-19 09:21:36 +0530

    create employee list

---
+++ src/components/Employee-list.component.js
@@ -0,0 +1,134 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+import { Button } from 'react-bootstrap';
+
+const Employee = props => ( <
+    tr >
+    <
+    td > { props.Employee.EmployeeID } </td> <
+    td > { props.Employee.EmployeeName } </td> <
+    td > { props.Employee.Address } </td> <
+    td > { props.Employee.Telephone } </td> <
+    td > { props.Employee.Email } </td> <
+    td > { props.Employee.Type } </td> <
+    td > { props.Employee.Discription } </td> <
+    td >
+    <
+    Link to = { "/edit/" + props.Employee._id } > Edit </Link> | <a href=" " onClick={() => { props.deleteEmployee(props.Employee._id) }}>Delete</a > </
+    td > </tr> 
+)
+
+export default class EmployeeList extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            Employee: []
+        };
+    }
+
+    
+
+    render() {
+        return ( <
+            div className = "container" >
+    
+            <div style = {
+                { float: 'none'}
+            } > 
+           
+            </div>  <br/>
+            
+            <
+            div className = "row" >
+            <
+            div className = "col-lg-9 mt-2 mb-2" >
+            <
+            h4 > ALL EMPLOYEES </h4> </
+            div > 
+            <
+            div className = "col-lg-3 mt-2 mb-2 text-right" style = {
+                { float: 'none' }
+            } >
+
+            <
+            Link to = "/create" >
+            <button type="button" class="btn btn-success" variant = "primary" style={{
+                background: "rgb(42 57 177)",
+    padding:"10px 20px"
+            }}> Add Employee </button>
+            </
+            Link >
+            </div>
+            <
+            div className = "col-lg-3 mt-2 mb-2" >
+            <
+            input className = "form-control"
+            type = "search"
+            placeholder = "Search by Employee Name"
+            name = "searchQuery"
+            onChange = { this.handleSearchArea } >
+            </
+            input> </
+            div > </
+            div>
+
+            <
+            table class="table table-bordered bg-light" >
+            <
+            thead className = "thead-dark" >
+            <
+            tr >
+            <
+            th className='align-middle'> Employee ID </th> <
+            th className='align-middle'> Employee Name </th> <
+            th className='align-middle'> Employee Address </th> <
+            th className='align-middle'> Telephone </th> <
+            th className='align-middle'> E mail </th> <
+            th className='align-middle'> Employee Type </th> <
+            th className='align-middle'> Brief Discription of Employee </th> <
+            th className='align-middle'> Actions </th> </
+            tr > </
+            thead> <
+            tbody >
+            
+             {
+                this.state.Employee.map(props =>
+                    <
+                    tr key = { props.EmployeeID } >
+                    
+                    <td > { props.EmployeeID } </td>  <
+                    td > { props.EmployeeName } </td>  <
+                    td > { props.Address } </td>  <
+                    td > { props.Telephone } </td>  < 
+                    td > { props.Email } </td>  <  
+                    td > { props.Type } </td>  < 
+                    td > { props.Discription } </td>  <  
+
+                    td className='text-center'>
+                    <
+                    Link to = { "/edit/" + props._id } className='mb-3 d-block'>  <Button variant = "info btn-sm"> Update </Button> </Link>  
+                    <a href="" onClick={() => { this.deleteEmployee(props._id) }}> <Button variant = "danger btn-sm"> Delete </Button> </a > 
+                    </
+                    td >
+
+                    </tr>
+                )
+
+            }
+
+            </tbody> </
+            table >
+
+            
+
+            </div>
+        )
+    }
+}
+
+
+
+
+
